01f8a4275c6b05f422f3b03fc8213a1b8d989816
Sarah Lin - TW (IT2) <SarahLin@supermicro.com>
2019-12-26 10:58:59 +0800

    commit by cmd git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'demo1';
+  title = 'Demo1';
+  name = 'sarah';
+  cnt = 0;
+  changeTitle(altKey: boolean) {
+    if (altKey) {
+      this.title = '!!!The Sarah Web';
+    }
+  }
+  ChangeCnt($event)
+  {
+    this.cnt = $event.target.value.length+1;
+    console.log($event);
+    console.log($event.target);
+    console.log($event.target.value);
+    console.log($event.target.value.length);
+  }
+  ResetCnt($event){
+    this.cnt = 0;
+    $event.target.value='';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page2Component } from './page2/page2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page2Component
   ],
   imports: [
     BrowserModule,
